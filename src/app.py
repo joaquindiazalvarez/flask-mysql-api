e2851a29d2f6edc672eea2ad10b6e3333d970cf9
joaquindiazalvarez <joaquindiazalvarez@gmail.com>
2022-07-12 03:00:53 -0400

    updating

--- src/app.py
+++ src/app.py
@@ -1,10 +1,30 @@
 from flask import Flask
+from flask_mysqldb import MySQL
+
+from config import config
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
-def index():
-    return "holamundo"
+def listar_cursos():
+    try:
+        cursor=conexion.connection.cursor()
+        sql = "SELECT codigo, nombre, creditos FROM curso"
+        cursor.execute(sql)
+        datos=cursor.fetchall()
+        print(datos)
+        return "Cursos listados!"
+    except Exception as ex:
+        return "Error"
+
+def pagina_no_encontrada(error):
+    return "<h1>La página que intentas buscar no existe ...</h1>"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.config.from_object(config['development'])    
+    app.register_error_handler(404, pagina_no_encontrada) 
+    app.run()
+    
+    
---
+++ src/config.py
@@ -0,0 +1,13 @@
+class DevelopmentConfig():
+    DEBUG = True
+    MYSQL_HOST = 'cornelius'
+    MYSQL_PASSWORD = '123456'
+    MYSQL_DB = 'cornelius'
+
+config = {
+        'development':DevelopmentConfig
+        }
+
+
+
+
